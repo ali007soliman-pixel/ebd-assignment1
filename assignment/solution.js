e7b799b219761a25850db72a26ab8895f5775419
Ali Osama <ali.shehenna@student.giu-uni.de>
2026-09-23 04:39:56 +0300

    Add solution

---
+++ assignment/solution.js
@@ -0,0 +1,9 @@
+const heroName = "Ali Osama";
+const studentId = 16003128;
+const power = studentId * 2;
+const warcry = "By Alexandria, the Sphinx Wraith flees!";
+
+console.log(heroName);
+console.log(studentId);
+console.log(power);
+console.log(warcry);
